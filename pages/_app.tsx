160ac3c52c3fad33117e23c0d0dd7f566ff39fd2
chaluemwut <chaluemwut.dev@myorder.ai>
2023-12-07 11:00:46 +0700

    fix app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
+import type { AppProps } from 'next/app'
 import './styles.css'
 import 'bootstrap/dist/css/bootstrap.css'
-
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+ 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
