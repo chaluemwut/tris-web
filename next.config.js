7a1edc895d18f9f840affca91cb6c3ae32f6389e
chaluemwut <chaluemwut.dev@myorder.ai>
2023-12-10 14:54:51 +0700

    #

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     env: {
         // apiURL: 'http://localhost:10000'
         // apiURL: 'https://tris-api.onrender.com'
-        apiURL: 'http://35.240.215.47'
+        apiURL: 'http://35.240.215.47/api'
     }
 }
 
