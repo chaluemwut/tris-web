865f5f410bd5a10d2d3fcc85b6f09b7efb18b116
chaluemwut <chaluemwut.dev@myorder.ai>
2023-12-13 11:48:49 +0700

    #

--- components/screens/project/ProjectDetailComponent.tsx
+++ components/screens/project/ProjectDetailComponent.tsx
@@ -1,5 +1,6 @@
 import HeaderContent from "../../HeaderContent";
 import projectHeaderImage from '../../../public/static/images/project_detail_header.png';
+import peopleCircle from '../../../public/static/images/people_circle_icon.svg';
 import Image from "next/image";
 
 export default function ProjectDetailComponent() {
@@ -8,7 +9,7 @@ export default function ProjectDetailComponent() {
         <div>
             <div className="row">
                 <div>
-                    <div className="card" style={{ marginLeft: '30px', marginRight: '30px', marginTop: '40px'}}>
+                    <div className="card" style={{ marginLeft: '30px', marginRight: '30px', marginTop: '40px' }}>
                         <div style={{ width: '100%', height: '200px', position: 'relative' }}>
                             <Image src={projectHeaderImage.src} alt="Project header" layout='fill' objectFit='cover' />
                         </div>
@@ -28,10 +29,12 @@ export default function ProjectDetailComponent() {
                                 </div>
                             </div>
                         </div>
-                        <div style={{marginBlock: '20px'}}>
+                        <div style={{ marginBlock: '20px' }}>
                             <div>
                                 <div style={{
-                                    display: 'flex', justifyContent: 'space-between',
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
                                     marginLeft: '20px', marginRight: '20px'
                                 }}>
                                     <div>
@@ -50,11 +53,50 @@ export default function ProjectDetailComponent() {
                                                 Invite People <i className="bi bi-person-plus-fill"></i>
                                             </div>
                                         </button>
+                                        <button className="btn buttom-radius" style={{ marginLeft: '10px' }}>
+                                            <div className="inner-bottom">Private</div>
+                                        </button>
+                                        <button className="btn buttom-radius" style={{ marginLeft: '10px' }}>
+                                            <div className="inner-bottom">Project Card</div>
+                                        </button>
+                                        <button className="btn buttom-radius" style={{ marginLeft: '10px' }}>
+                                            <div className="inner-bottom">Gantt Chart</div>
+                                        </button>
+                                        <button className="btn buttom-radius" style={{ marginLeft: '10px' }}>
+                                            <div className="inner-bottom">
+                                                <i className="bi bi-chat-left-text-fill" style={{ color: '#5A6ACE' }}></i>
+                                                <span style={{ marginLeft: '5px' }}>45 Comments</span>
+                                            </div>
+                                        </button>
                                     </div>
                                     <div>
-                                        <button>cc</button>
+                                        <Image src={peopleCircle.src} width={100} height={100} alt="people" />
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="card" style={{ marginLeft: '30px', marginRight: '30px', marginTop: '40px' }}>
+                        <div style={{ marginLeft: '20px', marginRight: '20px', marginTop: '20px' }}>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <div>รายละเอียดโครงการ</div>
+                                <div><i className="bi bi-pencil-square"></i></div>
+                            </div>
+                            <div>
+                                <p style={{ marginLeft: '10px', marginTop: '10px' }}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumdolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                <div>วัตถุประสงค์โครงการ</div>
+                                <p>
+                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+                                    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                                    consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
+                                    minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliqui.
+                                </p>
                             </div>
                         </div>
                     </div>
