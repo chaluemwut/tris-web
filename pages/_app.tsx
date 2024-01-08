1abdb43557e69c2eb298502a3b1dab76640978a4
chaluemwut <chaluemwut.dev@myorder.ai>
2024-01-09 06:43:09 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,14 @@ import './styles.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import "@fortawesome/fontawesome-svg-core/styles.css"
-import { config } from "@fortawesome/fontawesome-svg-core"; 
+import { config } from "@fortawesome/fontawesome-svg-core";
 // config.autoAddCss = false;
+import { Kanit } from 'next/font/google'
+const inter = Kanit({ subsets: ['latin'], weight: ['200'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>)
 }
