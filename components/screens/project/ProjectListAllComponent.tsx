df6f53f79bd464d52a882ce991569464e464d1d6
chaluemwut <chaluemwut.dev@myorder.ai>
2024-02-06 10:52:05 +0700

    update

--- components/screens/project/ProjectListAllComponent.tsx
+++ components/screens/project/ProjectListAllComponent.tsx
@@ -1,24 +1,133 @@
 import { useRouter } from "next/router";
 import HeaderContent from "../../HeaderContent";
+import { LineSeriesChart } from "../../chartjs/LineSeriesChart";
+import { ProjectProgress } from "../../chartjs/ProjectProgress";
+import progress1 from '../../../public/static/images/list/progress1.png';
+import progress2 from '../../../public/static/images/list/progress2.png';
+import progress3 from '../../../public/static/images/list/progress3.png';
+import progress4 from '../../../public/static/images/list/progress4.png';
+import progress5 from '../../../public/static/images/list/progress5.png';
+import progress6 from '../../../public/static/images/list/progress6.png';
+import progress7 from '../../../public/static/images/list/progress7.png';
+import progress8 from '../../../public/static/images/list/progress8.png';
+import progress9 from '../../../public/static/images/list/progress9.png';
+import progress10 from '../../../public/static/images/list/progress10.png';
 
 export default function ProjectListAllComponent() {
     const { push } = useRouter()
+
+
+    const getImage = (index: number) => {
+        if (index == 0) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress1.src} />
+                <span>3%</span>
+            </div>
+        } else if (index == 1) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress2.src} />
+                <span>51%</span>
+            </div>
+        } else if (index == 2) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress3.src} />
+                <span>81%</span>
+            </div>
+        } else if (index == 3) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress4.src} />
+                <span>63%</span>
+            </div>
+        } else if (index == 4) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress5.src} />
+                <span>25%</span>
+            </div>
+        } else if (index == 5) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress6.src} />
+                <span>93%</span>
+            </div>
+        } else if (index == 6) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress7.src} />
+                <span>48%</span>
+            </div>
+        } else if (index == 7) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress8.src} />
+                <span>85%</span>
+            </div>
+        } else if (index == 8) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress9.src} />
+                <span>78%</span>
+            </div>
+        } else if (index == 9) {
+            return <div style={{ display: 'flex' }}>
+                <img width={100} src={progress10.src} />
+                <span>100%</span>
+            </div>
+        } else {
+            return <img width={100} src={progress1.src} />
+        }
+    }
 
     return (<div style={{ marginBottom: '20px' }}>
         <HeaderContent title="โครงการ" />
         <div>
             <div className="card" style={{ marginLeft: '10px', marginRight: '10px', marginTop: '20px' }}>
-                <div></div>
-                <div style={{ marginTop: '5px' }}>
+                <div style={{
+                    display: 'flex', justifyContent: 'space-between',
+                    alignItems: 'center',
+                    marginTop: '20px',
+                    marginBottom: '10px',
+                    marginLeft: '20px',
+                    marginRight: '20px'
+                }}>
+                    <div>
+                        <span>รายละเอียดโครงการ</span>
+                    </div>
+                    <div style={{ display: 'flex' }}>
+                        <select style={{
+                            color: 'white', backgroundColor: '#5A6ACE',
+                            borderRadius: '5px', height: '35px',
+                            borderColor: '#5A6ACE'
+                        }}>
+                            <option>วัตถุประสงค์การจัดตั้งที่ 1</option>
+                        </select>
+                        <select style={{
+                            marginLeft: '10px', color: 'white', backgroundColor: '#5A6ACE',
+                            borderRadius: '5px', height: '35px',
+                            borderColor: '#5A6ACE'
+                        }}>
+                            <option>ยุทธศาตร์ที่ 1</option>
+                        </select>
+                        <select style={{
+                            marginLeft: '10px', color: 'white', backgroundColor: '#5A6ACE',
+                            borderRadius: '5px', height: '35px',
+                            borderColor: '#5A6ACE'
+                        }}>
+                            <option>แผนงาน 1</option>
+                        </select>
+                    </div>
+                </div>
+                <div style={{ marginTop: '5px', marginLeft: '20px', marginRight: '20px' }}>
                     <table className="table table-striped">
                         <thead>
                             <tr>
                                 <th className="col list-table-header">#</th>
                                 <th className="col list-table-header">รหัสโครงการ</th>
                                 <th className="col list-table-header">ชื่อโครงการ</th>
-                                <th className="col list-table-header">หน่วยงาน</th>
-                                <th className="col list-table-header">งบประมาณ</th>
+                                <th className="col list-table-header">หน่วยงานเจ้าของ
+                                    โครงการ ภายใต้
+                                    สังกัด สทร</th>
+                                <th className="col list-table-header">
+                                    <div>งบประมาณ</div>
+                                    <div>(บาท)</div>
+                                </th>
                                 <th className="col list-table-header">สถานะโครงการ</th>
+                                <th className="col list-table-header">ร้อยละ ความคืบหน้า</th>
                                 <th className="col list-table-header"></th>
                                 <th className="col list-table-header"></th>
                             </tr>
@@ -85,7 +194,7 @@ export default function ProjectListAllComponent() {
                                     'name': `โครงการพัฒนามาตรฐานการกำกับดูแลเพื่อสนับสนุนการดำเนินงานตามกฎหมายการขนส่งทางราง`,
                                     'price': '20,000,000',
                                     'status': ''
-                                },                                
+                                },
                             ].map((e, index) => <tr
                                 style={{ cursor: 'pointer' }}
                                 onClick={() => {
@@ -97,6 +206,9 @@ export default function ProjectListAllComponent() {
                                 <td>สทร.</td>
                                 <td>{e.price}</td>
                                 <td>{e.status}</td>
+                                <td>
+                                    {getImage(index)}
+                                </td>
                                 <td>
                                     <button className="btn" style={{ backgroundColor: '#F4694C', color: 'white' }}>Message Board</button>
                                     <button className="btn" style={{ backgroundColor: '#5A6ACE', marginLeft: '5px', color: 'white' }}>Task</button>
